18c83d6904677f731a4d3a07b6fbc6e1a43ceb5b
Alexander Skjolden <alex@plastboks.net>
2020-01-24 10:48:26 +0100

    updates the app for the kommunesammenslåing...
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

---
+++ README.md
@@ -0,0 +1,11 @@
+# Folloren webservice
+
+Small app for interacting with the backend service at folloren.no
+
+## Example usage
+
+Query is the actual address to lookup. Use folloren.no as a reference.
+Municipality can be [3020, 3021, 3022, 3023]
+Trash types are [Papir, Restavfall]
+
+http://localhost:5000/?query=...&municipality=...&trash_type=...
--- app.py
+++ app.py
@@ -2,21 +2,26 @@ from flask import Flask
 from flask import request
 from flask import abort
 from flask import jsonify
+from flask_caching import Cache
+
 import requests
 import json
 
 app = Flask(__name__)
+cache = Cache(config={'CACHE_TYPE': 'simple'})
+cache.init_app(app)
+
 user_agent = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0"
 referer = "http://www.folloren.no/"
 origin = "http://www.folloren.no"
-municipality = "213"
 app_key = "AE13DEEC-804F-4615-A74E-B4FAC11F0A30"
+cache_timeout = 3600
 
-
-def get_search(string):
+@cache.cached(timeout=cache_timeout, key_prefix='get_search')
+def get_search(string, municipality):
 
     main_url = "https://services.webatlas.no/GISLINE.Web.Services.Search.SOLR3.0/Service.svc/json/addressWeighted"
-    weighted_municipality = "0%s" % municipality
+    weighted_municipality = municipality
     client_id = "Android-Renovasjon-0%s" % municipality
 
     url = "%s?searchString=%s" \
@@ -41,7 +46,8 @@ def get_search(string):
     return json.loads(requests.get(url, headers=headers).text)
 
 
-def get_dates(road_name, road_code, house):
+@cache.cached(timeout=cache_timeout, key_prefix='get_dates')
+def get_dates(municipality, road_name, road_code, house):
 
     host = "norkartrenovasjon.azurewebsites.net"
     main_url = "https://norkartrenovasjon.azurewebsites.net/proxyserver.ashx"
@@ -65,7 +71,8 @@ def get_dates(road_name, road_code, house):
     return json.loads(requests.get(url, headers=headers).text)
 
 
-def enrich_data(data):
+@cache.cached(timeout=cache_timeout, key_prefix='enrich_data')
+def enrich_data(municipality, data):
 
     main_url = "https://norkartrenovasjon.azurewebsites.net/proxyserver.ashx"
     server_arg = "https://komteksky.norkart.no/komtek.renovasjonwebapi/api/fraksjoner/"
@@ -94,20 +101,28 @@ def enrich_data(data):
     return data
 
 
+@cache.cached(timeout=cache_timeout)
 @app.route("/", methods=['GET'])
 def hello():
     query = request.args.get('query', '')
+    municipality = request.args.get('municipality')
+    trash_type = request.args.get('trash_type', '')
 
     if not query:
         abort(400)
 
-    search_data = get_search(query)
+    search_data = get_search(query, municipality)
     road_code = search_data["AddressSearchResult"]["Roads"][0]["Id"]
     road_name = search_data["AddressSearchResult"]["Roads"][0]["RoadName"]
     house = search_data["AddressSearchResult"]["Roads"][0]["Addresses"][0]["House"]
 
-    data = get_dates(road_name, road_code, house)
-    enriched = enrich_data(data)
+    data = get_dates(municipality, road_name, road_code, house)
+    enriched = enrich_data(municipality, data)
+
+    if trash_type:
+        enriched = list(filter(lambda x: x["Avfallstype"] == trash_type, enriched))
+        enriched = enriched[0]
+
     return jsonify(enriched)
 
 
